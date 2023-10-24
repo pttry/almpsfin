12759df464d9a76ef05186dd81d56ba3fb051105
Juho Alasalmi <juho.alasalmi@ptt.fi>
2023-10-24 06:54:24 +0300

    use statfidata package

--- data-raw/luo_statfin_data.R
+++ data-raw/luo_statfin_data.R
@@ -2,28 +2,15 @@ data(taulut)
 
 for(i in 1:dim(taulut)[1]) {
 
-url <- paste0("https://pxnet2.stat.fi/PXWeb/api/v1/fi/", taulut$table_location[i])
+url <- statfidata::statfi_parse_url(taulut$table_location[i])
 query <- taulut$query[i][[1]]
+data <- statfidata::get_statfi_data(url = url, query = query)
 
-px_data <- pxweb::pxweb_get(url = url, query = query)
-data <- as.data.frame(px_data,
-                      column.name.type = "code",
-                      variable.value.type = "code") |>
-        statfitools::clean_times2() |>
-        dplyr::select(-all_of(names(query)[query == "SSS"])) |>
-        tidyr::pivot_longer(where(is.numeric)) |>
-        statfitools::clean_names() |>
-        rename(tiedot = name)
-
-
-data_name <- sapply(url,
-                    function(x) {paste(stringr::str_match(x, "statfin_\\s*(.*?)\\s*pxt_\\s*(.*?)\\s*.px")[,2:3], collapse = "")},
-                    USE.NAMES = FALSE)
-
+data_name <- statfidata::get_table_code(url)
 assign(data_name, data)
+
 do.call("use_data", list(as.name(data_name), overwrite = TRUE))
 
 }
-
 
 
